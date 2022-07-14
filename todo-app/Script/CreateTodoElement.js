26758d47472bb2a3ee64959a2979b0588406534c
DevNitro5 <98446102+DevNitro5@users.noreply.github.com>
2022-07-14 15:12:57 +0545

    Update CreateTodoElement.js

--- todo-app/Script/CreateTodoElement.js
+++ todo-app/Script/CreateTodoElement.js
@@ -63,4 +63,3 @@ export default function createTodoElement(...args) {
   return li;
 }
 
-// https://thewebdev.info/2021/09/12/how-to-append-multiple-child-elements-with-javascript/#:~:text=Elements%20with%20JavaScript-,To%20append%20multiple%20child%20elements%20with%20JavaScript,can%20use%20the%20append%20method.&text=Then%20we%20write%3A,')%20const%20listItem%20%3D%20document.
